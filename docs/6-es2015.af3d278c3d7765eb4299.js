ca5554cf8d615a3c6ac8aef1839b07eee8ea10c3
luiz carlos wagner dos santos <luizcarloswagner@gmail.com>
2020-07-20 17:30:22 -0300

    correcao, load module

---
+++ docs/6-es2015.af3d278c3d7765eb4299.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{GSjH:function(n,e,o){"use strict";o.r(e),o.d(e,"ExampleAModule",(function(){return a}));var t=o("ofXK"),r=o("fXoL"),c=o("b7mO"),u=o("jAig");o("c/MQ");let a=(()=>{class n{}return n.ngModuleDef=r.Db({type:n}),n.ngInjectorDef=r.Cb({factory:function(e){return new(e||n)},imports:[[t.b,c.a,u.a]]}),n})()}}]);
